2611a7abaf8b2ffcc240016c6f881246c71f910c
zZDANTI <99105200+zZDANTI@users.noreply.github.com>
2024-04-17 15:16:09 +0200

    view company

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 import PrimeVue from "primevue/config";
 import 'primevue/resources/themes/aura-light-green/theme.css'
 
+
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
