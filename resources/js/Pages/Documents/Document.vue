a9da21b2d8c35299b7ab57a1b75061aa62b91d45
JansTopete <janstopete@gmail.com>
2024-08-07 14:23:29 +0200

    Autofirma, change argument format

--- resources/js/Pages/Documents/Document.vue
+++ resources/js/Pages/Documents/Document.vue
@@ -1160,7 +1160,7 @@ export default {
                 function errorCallback(type,message) { console.log("ERR"); }
 
                 let dataB64 = AutoScript.getBase64FromText(data);
-                AutoScript.sign('sign', (dataB64 != undefined && dataB64 != null && dataB64 != "") ? dataB64 : null, "SHA512withRSA", "XAdES", "", null, successCallback, errorCallback);
+                AutoScript.sign('sign', (dataB64 != undefined && dataB64 != null && dataB64 != "") ? dataB64 : null, "SHA512withRSA", "AUTO", "", null, successCallback, errorCallback);
             }
         },
 
